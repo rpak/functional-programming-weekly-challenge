999861743f7efe3e50f15349fb21bc13763f39e0
Rob Pak <rob@carbonfive.com>
2014-08-26 08:50:48 -0700

    wip

---
+++ Week004/rpak/bst.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/ruby
+
+class BST
+  def self.create(nodes)
+  end
+
+  def self.insert(key, value, tree)
+
+  end
+
+  def self.update(key, value, tree)
+
+  end
+end
+
+m1 = BST.create([["name", "erin"]])
+m2 = BST.insert("age", 32, m1)
+m3 = BST.update("age", 33, m2)
+
+p "done"
